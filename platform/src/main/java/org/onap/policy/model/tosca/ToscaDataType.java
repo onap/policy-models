237dd48cacfeda0b39ec1c09ce23fe59787e9e14
Chenfei Gao <cgao@research.att.com>
2019-02-22 20:10:11 -0500

    Add POJOs to support policy design API
    Includes:
    a) Added POJOs for TOSCA policy type
    b) Added POJOs for TOSCA policy
    c) Added POJOs for TOSCA custom data type
    d) Added junit tests for POJOs
    
    Issue-ID: POLICY-1515
    Change-Id: I230b32339c60b7cd3c0b66adfc3f2b1bb38265c5
    Signed-off-by: Chenfei Gao <cgao@research.att.com>

---
+++ platform/src/main/java/org/onap/policy/model/tosca/ToscaDataType.java
@@ -0,0 +1,70 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP Policy Model
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.model.tosca;
+
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+import java.util.Map;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Class to represent custom data type in TOSCA definition
+ *
+ * @author Chenfei Gao (cgao@research.att.com)
+ *
+ */
+@ToString
+public class ToscaDataType {
+
+    @Getter
+    @Setter
+    @SerializedName("derived_from")
+    private String derivedFrom;
+
+    @Getter
+    @Setter
+    @SerializedName("version")
+    private String version;
+
+    @Getter
+    @Setter
+    @SerializedName("metadata")
+    private Map<String, String> metadata;
+
+    @Getter
+    @Setter
+    @SerializedName("description")
+    private String description;
+
+    @Getter
+    @Setter
+    @SerializedName("constraints")
+    private List<ToscaConstraint> constraints;
+
+    @Getter
+    @Setter
+    @SerializedName("properties")
+    private List<Map<String, ToscaProperty>> properties;
+}
---
+++ platform/src/main/java/org/onap/policy/model/tosca/ToscaPolicyType.java
@@ -0,0 +1,75 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP Policy Model
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.model.tosca;
+
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+import java.util.Map;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Class to represent the policy type in TOSCA definition
+ *
+ * @author Chenfei Gao (cgao@research.att.com)
+ *
+ */
+@ToString
+public class ToscaPolicyType {
+
+    @Getter
+    @Setter
+    @SerializedName("derived_from")
+    private String derivedFrom;
+
+    @Getter
+    @Setter
+    @SerializedName("version")
+    private String version;
+
+    @Getter
+    @Setter
+    @SerializedName("metadata")
+    private Map<String, String> metadata;
+
+    @Getter
+    @Setter
+    @SerializedName("description")
+    private String description;
+
+    @Getter
+    @Setter
+    @SerializedName("properties")
+    private List<Map<String, ToscaProperty>> properties;
+
+    @Getter
+    @Setter
+    @SerializedName("targets")
+    private List<String> targets;
+
+    @Getter
+    @Setter
+    @SerializedName("triggers")
+    private List<Map<String, ToscaTrigger>> triggers;
+}
