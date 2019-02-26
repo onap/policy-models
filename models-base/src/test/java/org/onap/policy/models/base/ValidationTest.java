20b07b2814d347e419985626b10ab4e56dbdd75f
liamfallon <liam.fallon@est.tech>
2019-02-26 13:57:39 +0000

    Add basic model object concepts
    This review introduces the basic concepts that all model objects
    inherit from. Using this approach, all concepts that inherit from
    these types can use standardized DAO handling and marrshal/unmarshal
    handling
    
    This approach is a more generic version of the approach used in the
    APEX PDP for model handling. The APEX model handling will inherit this
    module.
    
    Issue-ID: POLICY-1264
    Change-Id: I35b76659ab66cf3f33bee59a5528e49c7edf7796
    Signed-off-by: liamfallon <liam.fallon@est.tech>

---
+++ models-base/src/test/java/org/onap/policy/models/base/ValidationTest.java
@@ -0,0 +1,90 @@
+/*
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2016-2018 Ericsson. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.models.base;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.onap.policy.models.base.PfValidationResult.ValidationResult;
+
+public class ValidationTest {
+
+    @Test
+    public void test() {
+        PfValidationResult result = new PfValidationResult();
+        PfConceptKey pfKeyey = new PfConceptKey("PK", "0.0.1");
+        result = pfKeyey.validate(result);
+
+        assertNotNull(result);
+        assertTrue(result.isOk());
+        assertTrue(result.isValid());
+        assertEquals(PfValidationResult.ValidationResult.VALID, result.getValidationResult());
+        assertNotNull(result.getMessageList());
+
+        PfValidationMessage vmess0 = new PfValidationMessage(PfConceptKey.getNullKey(), PfConceptKey.class,
+                        ValidationResult.VALID, "Some message");
+        result.addValidationMessage(vmess0);
+
+        assertTrue(result.isOk());
+        assertTrue(result.isValid());
+        assertEquals(PfValidationResult.ValidationResult.VALID, result.getValidationResult());
+        assertNotNull(result.getMessageList());
+        assertNotNull("hello", result.toString());
+
+        PfValidationMessage vmess1 = new PfValidationMessage(PfConceptKey.getNullKey(), PfConceptKey.class,
+                        ValidationResult.OBSERVATION, "Some message");
+        result.addValidationMessage(vmess1);
+
+        assertTrue(result.isOk());
+        assertTrue(result.isValid());
+        assertEquals(PfValidationResult.ValidationResult.OBSERVATION, result.getValidationResult());
+        assertNotNull(result.getMessageList());
+        assertNotNull("hello", result.toString());
+
+        PfValidationMessage vmess2 = new PfValidationMessage(PfConceptKey.getNullKey(), PfConceptKey.class,
+                        ValidationResult.WARNING, "Some message");
+        result.addValidationMessage(vmess2);
+
+        assertFalse(result.isOk());
+        assertTrue(result.isValid());
+        assertEquals(PfValidationResult.ValidationResult.WARNING, result.getValidationResult());
+        assertNotNull(result.getMessageList());
+        assertNotNull("hello", result.toString());
+
+        PfValidationMessage vmess3 = new PfValidationMessage(PfConceptKey.getNullKey(), PfConceptKey.class,
+                        ValidationResult.INVALID, "Some message");
+        result.addValidationMessage(vmess3);
+
+        assertFalse(result.isOk());
+        assertFalse(result.isValid());
+        assertEquals(PfValidationResult.ValidationResult.INVALID, result.getValidationResult());
+        assertNotNull(result.getMessageList());
+        assertNotNull("hello", result.toString());
+
+        assertEquals(PfValidationResult.ValidationResult.INVALID, result.getMessageList().get(3).getValidationResult());
+        assertEquals("Some message", result.getMessageList().get(3).getMessage());
+        assertEquals(PfConceptKey.class.getCanonicalName(), result.getMessageList().get(3).getObservedClass());
+        assertEquals(PfConceptKey.getNullKey(), result.getMessageList().get(3).getObservedKey());
+    }
+}
