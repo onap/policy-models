bae9c3bc53b3fec645095138ccd4f056b7e18cfb
liamfallon <liam.fallon@est.tech>
2019-03-04 15:09:09 +0000

    Add DAO module for Models
    Issue-ID: POLICY-1195
    Change-Id: I2d15b00fcb50ea92746ab7c5a87b57efa07196fe
    Signed-off-by: liamfallon <liam.fallon@est.tech>

---
+++ models-dao/src/main/java/org/onap/policy/models/dao/PfDaoFactory.java
@@ -0,0 +1,70 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2019 Nordix Foundation.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.models.dao;
+
+import org.onap.policy.common.utils.validation.Assertions;
+import org.onap.policy.models.base.PfModelException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This factory class returns a Policy Framework DAO for the configured persistence mechanism. The
+ * factory uses the plugin class specified in {@link DaoParameters} to instantiate a DAO instance.
+ *
+ * @author Liam Fallon (liam.fallon@ericsson.com)
+ */
+public class PfDaoFactory {
+    // Get a reference to the logger
+    private static final Logger LOGGER = LoggerFactory.getLogger(PfDaoFactory.class);
+
+    /**
+     * Return a Policy Framework DAO for the required Policy Framework DAO plugin class.
+     *
+     * @param daoParameters parameters to use to read the database configuration information
+     * @return the Policy Framework DAO
+     * @throws PfModelException on invalid JPA plugins
+     */
+    public PfDao createPfDao(final DaoParameters daoParameters) throws PfModelException {
+        Assertions.argumentOfClassNotNull(daoParameters, PfModelException.class,
+                "Parameter \"daoParameters\" may not be null");
+
+        // Get the class for the DAO using reflection
+        Object pfDaoObject = null;
+        try {
+            pfDaoObject = Class.forName(daoParameters.getPluginClass()).newInstance();
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+            String errorMessage =
+                    "Policy Framework DAO class not found for DAO plugin \"" + daoParameters.getPluginClass() + "\"";
+            LOGGER.error(errorMessage, e);
+            throw new PfModelException(errorMessage, e);
+        }
+
+        // Check the class is a Policy Framework DAO
+        if (!(pfDaoObject instanceof PfDao)) {
+            String errorMessage = "Specified DAO plugin class \"" + daoParameters.getPluginClass()
+                    + "\" does not implement the PfDao interface";
+            LOGGER.error(errorMessage);
+            throw new PfModelException(errorMessage);
+        }
+
+        return (PfDao) pfDaoObject;
+    }
+}
