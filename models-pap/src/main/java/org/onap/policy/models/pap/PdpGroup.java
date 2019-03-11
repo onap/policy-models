c0deb20ec3a0f3ba05957b4260f1f5c5bd3220d2
ramverma <ram.krishna.verma@est.tech>
2019-03-11 15:48:07 +0000

    Adding models-pap module to policy/models
    1) Adding models-pap module to policy/models for storing models required
    by PAP REST API's.
    2) Adding test cases.
    
    Change-Id: I4e8c38f1ebe96e7a50a188f19f4f4a5bed835f99
    Issue-ID: POLICY-1541
    Signed-off-by: ramverma <ram.krishna.verma@est.tech>

---
+++ models-pap/src/main/java/org/onap/policy/models/pap/PdpGroup.java
@@ -0,0 +1,48 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2019 Nordix Foundation.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.models.pap;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import org.onap.policy.pdp.common.enums.PdpState;
+
+/**
+ * Class to represent a PDPGroup, which groups multiple PDPSubGroup entities together for a particular domain.
+ *
+ * @author Ram Krishna Verma (ram.krishna.verma@est.tech)
+ */
+@Getter
+@Setter
+@ToString
+public class PdpGroup {
+
+    private String name;
+    private String version;
+    private PdpState state;
+    private String description;
+    private String properties;
+    private List<PdpSubGroup> pdpSubgroups;
+
+}
