8cf69ef4f8d4b6d5c90350e4036777350c45f289
Pamela Dragosh <pdragosh@research.att.com>
2019-03-16 17:17:37 -0400

    Add decision models
    Adding various decision models and their
    serialization handler classes
    
    Issue-ID: POLICY-1440
    Change-Id: I35abfd9745a86b822f658ec3a512b7355cabf068
    Signed-off-by: Pamela Dragosh <pdragosh@research.att.com>

---
+++ models-decisions/src/main/java/org/onap/policy/models/decisions/concepts/DecisionResponse.java
@@ -0,0 +1,39 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP Policy Decision Models
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.models.decisions.concepts;
+
+import java.util.List;
+import java.util.Map;
+
+import lombok.Data;
+
+/**
+ * Generic class for handling Decision Response objects.
+ *
+ * @author pameladragosh
+ *
+ */
+@Data
+public class DecisionResponse {
+
+    private String  errorMessage;
+    private List<Map<String, Object>> policies;
+}
