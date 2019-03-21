a9e7f633e31450acf21cf05cdcd358220834ef16
Jim Hahn <jrh3@att.com>
2019-03-21 09:54:30 -0400

    Add models-pdp to models repo
    Also changed supportedPolicyTypes to PfConceptKey.
    
    Updated licenses.
    
    Removed trailing whitespace from pom.
    
    Provide default request id.
    
    Rebased to fix merge conflict.
    
    Added SupportedPolicyType class.
    
    Change-Id: Id65f769c2f308c6b56e79978bd50b84f2e0b3d02
    Issue-ID: POLICY-1542
    Signed-off-by: Jim Hahn <jrh3@att.com>

---
+++ models-pdp/src/main/java/org/onap/policy/models/pdp/concepts/PdpStateChange.java
@@ -0,0 +1,52 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2019 Nordix Foundation.
+ *  Modifications Copyright (C) 2019 AT&T Intellectual Property.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.models.pdp.concepts;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.onap.policy.models.pdp.enums.PdpMessageType;
+import org.onap.policy.models.pdp.enums.PdpState;
+
+/**
+ * Class to represent the PDP_STATE_CHANGE message that PAP will send to either PDPGroup/Subgroup or a PDP.
+ *
+ * @author Ram Krishna Verma (ram.krishna.verma@est.tech)
+ */
+@Getter
+@Setter
+@ToString
+public class PdpStateChange extends PdpMessage {
+
+    private String name;
+    private PdpState state;
+    private String pdpGroup;
+    private String pdpSubgroup;
+
+    /**
+     * Constructor for instantiating PdpStateChange class with message name.
+     *
+     */
+    public PdpStateChange() {
+        super(PdpMessageType.PDP_STATE_CHANGE);
+    }
+}
