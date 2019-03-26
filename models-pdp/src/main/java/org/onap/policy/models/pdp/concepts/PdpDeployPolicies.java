7ae7cf35d579d69e3cdaf165da5bf39646b37276
Jim Hahn <jrh3@att.com>
2019-03-26 13:49:51 -0400

    Add corresponding classes between PAP and PDP
    Some classes were in models-pap without corresponding classes in
    models-pdp and vice versa.  Added the classes.
    Also modified PdpSubGroup to subclass from ToscaEntityType.
    
    Missed a few more classes.
    
    Change-Id: I24c2a75cb163f72bf909e17dcc7efe7191bd1023
    Issue-ID: POLICY-1542
    Signed-off-by: Jim Hahn <jrh3@att.com>

---
+++ models-pdp/src/main/java/org/onap/policy/models/pdp/concepts/PdpDeployPolicies.java
@@ -0,0 +1,39 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP Policy Models
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.models.pdp.concepts;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Request deploy or update a set of policies using the <i>simple</i> PDP Group deployment
+ * REST API. Only the "name" and "policyVersion" fields of a Policy are used, and only the
+ * "name" field is actually required.
+ */
+@Getter
+@Setter
+@ToString
+public class PdpDeployPolicies {
+    private List<PolicyIdentOptVersion> policies;
+}
---
+++ models-pdp/src/main/java/org/onap/policy/models/pdp/concepts/SimpleResponse.java
@@ -0,0 +1,39 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP Policy Models
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.models.pdp.concepts;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Response returned when no extra output fields are needed.
+ */
+@Getter
+@Setter
+@ToString
+public class SimpleResponse {
+
+    /**
+     * Optional detailed message in error cases.
+     */
+    private String errorDetails;
+}
