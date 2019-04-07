918dd6fe5473577556a515723620843f467738a6
Jim Hahn <jrh3@att.com>
2019-04-07 10:50:27 -0400

    Clean up a couple of array lists
    Initialized initial array size for empty list.
    Don't create an extra copy of an array sublist.
    Add some junit tests to ensure that the return lists are mutable.
    
    Change-Id: I9c151eb50355a71f4d34ac46b41f8278cf913664
    Issue-ID: POLICY-1542
    Signed-off-by: Jim Hahn <jrh3@att.com>

--- models-base/src/main/java/org/onap/policy/models/base/PfObjectFilter.java
+++ models-base/src/main/java/org/onap/policy/models/base/PfObjectFilter.java
@@ -1,6 +1,7 @@
 /*-
  * ============LICENSE_START=======================================================
  *  Copyright (C) 2019 Nordix Foundation.
+ *  Modifications Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -84,6 +85,6 @@ public interface PfObjectFilter<T extends Comparable<T>> {
             filteredList.set(icur, lastElement);
         }
 
-        return new ArrayList<>(filteredList.subList(0, icur + 1));
+        return filteredList.subList(0, icur + 1);
     }
 }
