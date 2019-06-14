cb31b214e4e837fbf447524318926ed77563c5bf
Pamela Dragosh <pdragosh@research.att.com>
2019-06-14 13:14:32 -0400

    Fix copyright
    Missed on earlier reviews
    
    Issue-ID: POLICY-1855
    Change-Id: Ib172059e105e2940dce43f5da79b81dbd80f527d
    Signed-off-by: Pamela Dragosh <pdragosh@research.att.com>

--- models-interactions/model-simulators/src/main/java/org/onap/policy/simulators/GuardSimulatorJaxRs.java
+++ models-interactions/model-simulators/src/main/java/org/onap/policy/simulators/GuardSimulatorJaxRs.java
@@ -2,7 +2,7 @@
  * ============LICENSE_START=======================================================
  * simulators
  * ================================================================================
- * Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2017-2019 AT&T Intellectual Property. All rights reserved.
  * Modifications Copyright (C) 2019 Nordix Foundation.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
