04edc818a688ce9f587f222190e44370219c74c4
Jim Hahn <jrh3@att.com>
2019-10-14 11:34:15 -0400

    Add PAP Policy Notification classes
    Added classes that PAP will use to publish to DMaaP to indicate policy
    status changes (i.e., added/removed from PDPs).
    Modified serialized name of the policy identified to match other uses
    (e.g., PAP REST API calls).
    Added policy type, per review comments.
    
    Change-Id: I66131fa9ae0b63081f239e7f402b7fea65c434a5
    Issue-ID: POLICY-1841
    Signed-off-by: Jim Hahn <jrh3@att.com>

---
+++ models-pap/src/test/java/org/onap/policy/models/pap/concepts/PolicyNotificationTest.java
@@ -0,0 +1,60 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.models.pap.concepts;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+import org.onap.policy.common.utils.coder.CoderException;
+import org.onap.policy.common.utils.coder.StandardCoder;
+
+/**
+ * This only tests the methods that aren't already tested via TestModels.
+ */
+public class PolicyNotificationTest {
+
+    @Test
+    public void test() throws CoderException {
+        PolicyStatus statusAdd1 = new PolicyStatus();
+        statusAdd1.setSuccessCount(10);
+        PolicyStatus statusAdd2 = new PolicyStatus();
+        statusAdd2.setFailureCount(20);
+        List<PolicyStatus> add = Arrays.asList(statusAdd1, statusAdd2);
+
+        PolicyStatus statusDel1 = new PolicyStatus();
+        statusDel1.setIncompleteCount(30);
+        PolicyStatus statusDel2 = new PolicyStatus();
+        List<PolicyStatus> del = Arrays.asList(statusDel1, statusDel2);
+
+        // test constructor with arguments
+        PolicyNotification notify = new PolicyNotification(add, del);
+        assertSame(add, notify.getAdded());
+        assertSame(del, notify.getDeleted());
+
+        // encode & decode
+        StandardCoder coder = new StandardCoder();
+        PolicyNotification notify2 = coder.decode(coder.encode(notify), PolicyNotification.class);
+
+        // test equals() method (and verify encode/decode worked)
+        assertEquals(notify, notify2);
+    }
+}
