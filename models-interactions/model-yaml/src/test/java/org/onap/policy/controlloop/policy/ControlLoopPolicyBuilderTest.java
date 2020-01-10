1179e73c03eac7766771fee9d27d9d94b3ac6f0c
Jim Hahn <jrh3@att.com>
2020-01-10 17:52:57 -0500

    Remove deprecated methods from models
    Addressed the following issues related to the java 11 upgrade:
    - forClass().newInstance()
    - java.xml.bind no longer included in jdk
    - new mockito doesn't clear captured lists
    
    Issue-ID: POLICY-1406
    Signed-off-by: Jim Hahn <jrh3@att.com>
    Change-Id: Ia254c1ed21f847d1824e7abc22528975372ee2b6

--- models-interactions/model-yaml/src/test/java/org/onap/policy/controlloop/policy/ControlLoopPolicyBuilderTest.java
+++ models-interactions/model-yaml/src/test/java/org/onap/policy/controlloop/policy/ControlLoopPolicyBuilderTest.java
@@ -2,7 +2,7 @@
  * ============LICENSE_START=======================================================
  * policy-yaml unit test
  * ================================================================================
- * Copyright (C) 2017-2019 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2017-2020 AT&T Intellectual Property. All rights reserved.
  * Modifications Copyright (C) 2019 Nordix Foundation.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -341,7 +341,7 @@ public class ControlLoopPolicyBuilderTest {
                         PolicyResult.FAILURE,
                         PolicyResult.FAILURE_RETRIES,
                         PolicyResult.FAILURE_TIMEOUT);
-        assertTrue(builder.calculateTimeout().equals(new Integer(300 + 600)));
+        assertEquals(Integer.valueOf(300 + 600), builder.calculateTimeout());
     }
 
     @Test
