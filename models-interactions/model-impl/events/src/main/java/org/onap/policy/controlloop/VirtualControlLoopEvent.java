612b1fe75887bf41329016e186d69f09d8af427f
jhh <jorge.hernandez-herrero@att.com>
2020-01-14 22:48:45 -0600

    Added Time agnostic Onset and Abated classes
    These classes can be used for comparison of alarm skeletons
    independently of the time at which they were produced.
    
    Issue-ID: POLICY-2323
    Signed-off-by: jhh <jorge.hernandez-herrero@att.com>
    Change-Id: I85b9d6a429de56f056eb0a6caa9e4f90fbd68918
    Signed-off-by: jhh <jorge.hernandez-herrero@att.com>

--- models-interactions/model-impl/events/src/main/java/org/onap/policy/controlloop/VirtualControlLoopEvent.java
+++ models-interactions/model-impl/events/src/main/java/org/onap/policy/controlloop/VirtualControlLoopEvent.java
@@ -2,7 +2,7 @@
  * ============LICENSE_START=======================================================
  * controlloop
  * ================================================================================
- * Copyright (C) 2017-2019 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2017-2020 AT&T Intellectual Property. All rights reserved.
  * Modifications Copyright (C) 2019 Nordix Foundation.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -29,19 +29,21 @@ import java.util.Map;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
+@ToString(callSuper = true)
 public class VirtualControlLoopEvent extends ControlLoopEvent {
 
     private static final long serialVersionUID = -5752405682246066226L;
 
     @SerializedName("AAI")
-    private Map<String, String> aai = new HashMap<>();
-    private String payload;
-    private Instant closedLoopAlarmStart;
-    private Instant closedLoopAlarmEnd;
+    protected Map<String, String> aai = new HashMap<>();
+    protected String payload;
+    protected Instant closedLoopAlarmStart;
+    protected Instant closedLoopAlarmEnd;
 
     /**
      * Construct an instance from an existing instance.
