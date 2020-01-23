64b94bc432332f0ef2da76289968e8b5d9384e76
Jim Hahn <jrh3@att.com>
2020-01-23 18:56:40 -0500

    Actor prototype round #2
    Left original code intact so that it can continue to be used until
    everything has been converted to use the new approach.  Simply added
    new methods and classes.  (A few minor edits were required to the old
    code, e.g., added constructors to the Actor implementations).  Code
    to be removed is annotated with "TODO".
    
    This only contains one revised actor, SDNC.  This actor combines code
    from actor.sdnc, sdnc, and drools-applications.
    
    Added operation Builder and simplified generation of futures.  Modified
    the code to pass exceptions back via the future.
    Allow executor service to be overridden.
    Added ClosedLoopEventContext and moved the event and A&AI data inside,
    eliminating the need for ControlLoopAaiData.
    Modified code to throw exceptions when an actor or operation is not found,
    or if the operation manager has not been started.  This makes it easier to
    chain the calls together, using a single try/catch.
    Moved classes around into different packages.
    Modified to allow chaining of operations onto the end of an existing
    future.
    Added support for invoking an operation as a sub-operation, whose actions
    are appended to the parent's pipeline.
    Returned OperationOutcome instead of simple PolicyResult.
    Invoked callback when operation starts.
    Provided mechanism whereby operations can determine if the pipeline has
    been stopped.  This is independent of the Future.cancel() method.
    Broke futures into separate segments so that each segment can be
    controlled by its own timeout.
    Eliminated startPreProcessor(), as startOperation() will now be responsible
    for the whole operation, including retries.
    Handled exceptions from contained objects (e.g., from configure(), start())
    Implemented retries.
    Merged properties so that some can be specified at the actor level.
    Added target entity to operation parameters.
    Rename classes from Closed Loop to Control Loop, as they are generic.
    Modified the code to be more reliable about invoking the actor's start and
    complete callbacks.  Once the pipeline has been stopped, no further
    callbacks are invoked.
    Added preprocessor call to Operation Manager.
    Added ParameterValidationRuntimeException class to propagate
    validation results up.
    Renamed OperationManager to Operator.
    Refactor common code in junit.
    Added test for "future" return values.
    Moved stopPipeline and handleException up.
    Prevented deadlocks in pipeline controller.  Also modified it to
    automatically remove itself from its parent when it's stopped.
    Instead of returning a failed future, Params start() should throw
    an exception.
    Allow a pipeline's "future" to cancel the pipeline thus eliminating the
    need to pass a pipeline controller into Params.
    Removed attempt from Params.
    Need to adjust junits per latest round of changes.
    
    Issue-ID: POLICY-1625
    Signed-off-by: Jim Hahn <jrh3@att.com>
    Change-Id: I5beccd7c24cd4ee67b7a29b7d346afcd29c17cfc

---
+++ models-interactions/model-actors/actorServiceProvider/src/main/java/org/onap/policy/controlloop/actorserviceprovider/pipeline/PipelineControllerFuture.java
@@ -0,0 +1,153 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP
+ * ================================================================================
+ * Copyright (C) 2020 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.controlloop.actorserviceprovider.pipeline;
+
+import static org.onap.policy.controlloop.actorserviceprovider.Util.ident;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Pipeline controller, used by operations within the pipeline to determine if they should
+ * continue to run. If {@link #cancel(boolean)} is invoked, it automatically stops the
+ * pipeline.
+ */
+@NoArgsConstructor
+public class PipelineControllerFuture<T> extends CompletableFuture<T> {
+
+    private static final Logger logger = LoggerFactory.getLogger(PipelineControllerFuture.class);
+
+    private final FutureManager futures = new FutureManager();
+
+    /**
+     * Cancels and stops the pipeline, in that order.
+     */
+    @Override
+    public boolean cancel(boolean mayInterruptIfRunning) {
+        try {
+            logger.trace("{}: cancel future", ident(this));
+            return super.cancel(mayInterruptIfRunning);
+
+        } finally {
+            futures.stop();
+        }
+    }
+
+    /**
+     * Generates a function that, when invoked, will remove the given future. This is
+     * typically added onto the end of a pipeline via one of the
+     * {@link CompletableFuture#whenComplete(BiConsumer)} methods.
+     *
+     * @return a function that removes the given future
+     */
+    public <F> BiConsumer<T, Throwable> delayedRemove(Future<F> future) {
+        return (value, thrown) -> {
+            logger.trace("{}: remove future {}", ident(this), ident(future));
+            remove(future);
+        };
+    }
+
+    /**
+     * Generates a function that, when invoked, will remove the given listener. This is
+     * typically added onto the end of a pipeline via one of the
+     * {@link CompletableFuture#whenComplete(BiConsumer)} methods.
+     *
+     * @return a function that removes the given listener
+     */
+    public BiConsumer<T, Throwable> delayedRemove(Runnable listener) {
+        return (value, thrown) -> {
+            logger.trace("{}: remove listener {}", ident(this), ident(listener));
+            remove(listener);
+        };
+    }
+
+    /**
+     * Generates a function that, when invoked, will stop all pipeline listeners and
+     * complete this future. This is typically added onto the end of a pipeline via one of
+     * the {@link CompletableFuture#whenComplete(BiConsumer)} methods.
+     *
+     * @return a function that stops all pipeline listeners
+     */
+    public BiConsumer<T, Throwable> delayedComplete() {
+        return (value, thrown) -> {
+            if (thrown == null) {
+                logger.trace("{}: complete and stop future", ident(this));
+                complete(value);
+            } else {
+                logger.trace("{}: complete exceptionally and stop future", ident(this));
+                completeExceptionally(thrown);
+            }
+
+            futures.stop();
+        };
+    }
+
+    /**
+     * Adds a function whose return value is to be canceled when this controller is
+     * stopped. Note: if the controller is already stopped, then the function will not be
+     * executed.
+     *
+     * @param futureMaker function to be invoked in the future
+     */
+    public <F> Function<F, CompletableFuture<F>> add(Function<F, CompletableFuture<F>> futureMaker) {
+
+        return input -> {
+            if (!isRunning()) {
+                logger.trace("{}: discarded add", ident(this));
+                return new CompletableFuture<>();
+            }
+
+            CompletableFuture<F> future = futureMaker.apply(input);
+            add(future);
+
+            return future;
+        };
+    }
+
+    public <F> void add(Future<F> future) {
+        logger.trace("{}: add future {}", ident(this), ident(future));
+        futures.add(future);
+    }
+
+    public void add(Runnable listener) {
+        logger.trace("{}: add listener {}", ident(this), ident(listener));
+        futures.add(listener);
+    }
+
+    public boolean isRunning() {
+        return futures.isRunning();
+    }
+
+    public <F> void remove(Future<F> future) {
+        logger.trace("{}: remove future {}", ident(this), ident(future));
+        futures.remove(future);
+    }
+
+    public void remove(Runnable listener) {
+        logger.trace("{}: remove listener {}", ident(this), ident(listener));
+        futures.remove(listener);
+    }
+}
