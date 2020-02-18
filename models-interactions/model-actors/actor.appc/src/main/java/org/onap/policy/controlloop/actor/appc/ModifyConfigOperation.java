43cf828fcc152ad830222377bb61fb8a51a177fd
Jim Hahn <jrh3@att.com>
2020-02-18 12:26:34 -0500

    APPC Actor
    Issue-ID: POLICY-2363
    Signed-off-by: Jim Hahn <jrh3@att.com>
    Change-Id: I21908c9875aa1d2ad3678cb7613b5a3e1fada340

---
+++ models-interactions/model-actors/actor.appc/src/main/java/org/onap/policy/controlloop/actor/appc/ModifyConfigOperation.java
@@ -0,0 +1,75 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP
+ * ================================================================================
+ * Copyright (C) 2020 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.controlloop.actor.appc;
+
+import java.util.concurrent.CompletableFuture;
+import org.onap.aai.domain.yang.GenericVnf;
+import org.onap.policy.aai.AaiConstants;
+import org.onap.policy.aai.AaiCqResponse;
+import org.onap.policy.appc.Request;
+import org.onap.policy.controlloop.actor.aai.AaiCustomQueryOperation;
+import org.onap.policy.controlloop.actorserviceprovider.OperationOutcome;
+import org.onap.policy.controlloop.actorserviceprovider.impl.BidirectionalTopicOperator;
+import org.onap.policy.controlloop.actorserviceprovider.parameters.ControlLoopOperationParams;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ModifyConfigOperation extends AppcOperation {
+    private static final Logger logger = LoggerFactory.getLogger(ModifyConfigOperation.class);
+
+    public static final String NAME = "ModifyConfig";
+
+    /**
+     * Constructs the object.
+     *
+     * @param params operation parameters
+     * @param operator operator that created this operation
+     */
+    public ModifyConfigOperation(ControlLoopOperationParams params, BidirectionalTopicOperator operator) {
+        super(params, operator);
+    }
+
+    /**
+     * Ensures that A&AI customer query has been performed, and then runs the guard query.
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    protected CompletableFuture<OperationOutcome> startPreprocessorAsync() {
+        ControlLoopOperationParams cqParams = params.toBuilder().actor(AaiConstants.ACTOR_NAME)
+                        .operation(AaiCustomQueryOperation.NAME).payload(null).retry(null).timeoutSec(null).build();
+
+        // run Custom Query and Guard, in parallel
+        return allOf(() -> params.getContext().obtain(AaiCqResponse.CONTEXT_KEY, cqParams), this::startGuardAsync);
+    }
+
+    @Override
+    protected Request makeRequest(int attempt) {
+        AaiCqResponse cq = params.getContext().getProperty(AaiCqResponse.CONTEXT_KEY);
+
+        GenericVnf genvnf = cq.getGenericVnfByModelInvariantId(params.getTarget().getResourceID());
+        if (genvnf == null) {
+            logger.info("{}: target entity could not be found for {}", getFullName(), params.getRequestId());
+            throw new IllegalArgumentException("target vnf-id could not be found");
+        }
+
+        return makeRequest(attempt, genvnf.getVnfId());
+    }
+}
