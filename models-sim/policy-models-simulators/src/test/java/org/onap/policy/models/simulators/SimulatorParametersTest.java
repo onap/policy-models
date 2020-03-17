2ad5c48fc31fd13d9ac1fb174de8fc02929ee3de
Jim Hahn <jrh3@att.com>
2020-03-17 17:41:59 -0400

    Add docker file for all simulators
    Some CSITs may require multiple simulators.  This adds a class that
    will start all of the simulators.  A tarball is generated from which
    a docker image can be built.
    Added simulators for Topics: appc and appc-lcm.
    
    Issue-ID: POLICY-2434
    Signed-off-by: Jim Hahn <jrh3@att.com>
    Change-Id: Id7aa9cb5a5874f7b4185273ab0d2c074198554ff

---
+++ models-sim/policy-models-simulators/src/test/java/org/onap/policy/models/simulators/SimulatorParametersTest.java
@@ -0,0 +1,47 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP
+ * ================================================================================
+ * Copyright (C) 2020 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.models.simulators;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.io.File;
+import org.junit.Test;
+import org.onap.policy.common.parameters.BeanValidationResult;
+import org.onap.policy.common.utils.coder.CoderException;
+import org.onap.policy.common.utils.coder.StandardCoder;
+
+public class SimulatorParametersTest {
+
+    @Test
+    public void testValidate() throws CoderException {
+        // some fields missing
+        BeanValidationResult result = new SimulatorParameters().validate("InvalidParams");
+        assertFalse(result.isValid());
+        assertNotNull(result.getResult());
+
+        // everything populated
+        SimulatorParameters params = new StandardCoder().decode(new File("src/test/resources/simParameters.json"),
+                        SimulatorParameters.class);
+        assertNull(params.validate("ValidParams").getResult());
+    }
+}
