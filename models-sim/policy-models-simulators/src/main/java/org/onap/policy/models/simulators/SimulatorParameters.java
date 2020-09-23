1950621dec52e9256200be6c0a92ac8da18398bb
puthuparambil.aditya <aditya.puthuparambil@bell.ca>
2020-09-23 12:39:46 +0100

    Enhance gRPC Simulator:
    1.Make gRPC Simulator respond to different actions by generating the response on the basis of received request.
    2.Include CDS Simulator in policy-models-simulator
    3.Introduce a packages module in models-sim to package the docker image of policy-models-simulator which includes all the simulators(pdp-simulator to be added in future) in models.
    4.The Jenkins job changes to push this newly created Docker image may be taken in the next release.
    
    Issue-ID: POLICY-2828
    Signed-off-by: puthuparambil.aditya <aditya.puthuparambil@bell.ca>
    Change-Id: Ic1663bbe8205c64a8133ea0b0c28152f21d0732a

--- models-sim/policy-models-simulators/src/main/java/org/onap/policy/models/simulators/SimulatorParameters.java
+++ models-sim/policy-models-simulators/src/main/java/org/onap/policy/models/simulators/SimulatorParameters.java
@@ -3,6 +3,7 @@
  * ONAP
  * ================================================================================
  * Copyright (C) 2020 AT&T Intellectual Property. All rights reserved.
+ * Modifications Copyright (C) 2020 Bell Canada. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,6 +44,8 @@ public class SimulatorParameters {
      * the provider parameters.
      */
     private DmaapSimParameterGroup dmaapProvider;
+
+    private CdsServerParameters grpcServer;
 
     /**
      * Parameters for the REST server simulators that are to be started.
