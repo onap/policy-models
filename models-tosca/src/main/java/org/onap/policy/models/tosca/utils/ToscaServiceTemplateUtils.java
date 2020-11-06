041f5dd96839596f985a349f4435787d7a068183
Pamela Dragosh <pdragosh@research.att.com>
2020-11-06 08:16:56 -0500

    Fix upcoming checkstyle issues
    When we upgrade to oparent, these will fail as oparent upgrades
    to 8.37 of puppycrawl which fixes a few bugs that should have
    flagged these previously.
    
    Issue-ID: POLICY-2887
    Change-Id: Ia0ceab23ff3d5c177f657c5852115ab1b265c698
    Signed-off-by: Pamela Dragosh <pdragosh@research.att.com>

--- models-tosca/src/main/java/org/onap/policy/models/tosca/utils/ToscaServiceTemplateUtils.java
+++ models-tosca/src/main/java/org/onap/policy/models/tosca/utils/ToscaServiceTemplateUtils.java
@@ -1,6 +1,7 @@
 /*-
  * ============LICENSE_START=======================================================
  * Copyright (C) 2020 Nordix Foundation.
+ * Modifications Copyright (C) 2020 AT&T
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -52,7 +53,7 @@ public class ToscaServiceTemplateUtils {
      *
      * @param originalTemplate the original service template
      * @param fragmentTemplate the fragment being added to the original service template
-     * @return
+     * @return JpaToscaServiceTemplate
      */
     public static JpaToscaServiceTemplate addFragment(@NonNull final JpaToscaServiceTemplate originalTemplate,
             @NonNull final JpaToscaServiceTemplate fragmentTemplate) {
