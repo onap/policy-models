5b0a101fba78425b17f305b505f819ecc1acaef0
liamfallon <liam.fallon@est.tech>
2021-01-18 11:17:28 +0000

    Convert Model Exception to Runtime Exception
    Sometimes the exception thrown by the database providers has to be
    converted to a runtime exception, especiall if the database provider is
    running in a thread (called by the rum() method that does not return
    exceptions). It is convenient to have a constructor on the
    PfModelRuntimeException class that takes a PfModelException class as its
    argument, thus creating the runtime exception.
    
    Issue-ID: POLICY-2971
    Change-Id: Ib0b3678de531fd383753a1cd0ce47a06f4079ec0
    Signed-off-by: liamfallon <liam.fallon@est.tech>

--- models-base/src/test/java/org/onap/policy/models/base/ExceptionsTest.java
+++ models-base/src/test/java/org/onap/policy/models/base/ExceptionsTest.java
@@ -1,7 +1,7 @@
 /*-
  * ============LICENSE_START=======================================================
- *  Copyright (C) 2019 Nordix Foundation.
- *  Modifications Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2019, 2021 Nordix Foundation.
+ * Modifications Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -60,5 +60,9 @@ public class ExceptionsTest {
         assertEquals("Runtime Message\nIO runtime exception message",
                 String.join("\n", errorResponse.getErrorDetails()));
         assertEquals(key, re.getObject());
+
+        PfModelRuntimeException pfre = new PfModelRuntimeException(ae);
+        assertEquals(ae.getErrorResponse().getResponseCode(), pfre.getErrorResponse().getResponseCode());
+        assertEquals(ae.getMessage(), pfre.getMessage());
     }
 }
