bb0742e08c8b873b9f2890e8b445e4851d13a94f
Jim Hahn <jrh3@att.com>
2021-06-01 11:07:44 -0400

    Fix junits for new bean validation field names
    Issue-ID: POLICY-3333
    Change-Id: I339024677a2d090f61cc55d1b056b832c87d231a
    Signed-off-by: Jim Hahn <jrh3@att.com>

--- models-tosca/src/test/java/org/onap/policy/models/tosca/simple/concepts/JpaToscaServiceTemplateTest.java
+++ models-tosca/src/test/java/org/onap/policy/models/tosca/simple/concepts/JpaToscaServiceTemplateTest.java
@@ -1,7 +1,7 @@
 /*-
  * ============LICENSE_START=======================================================
  *  Copyright (C) 2019-2021 Nordix Foundation.
- *  Modifications Copyright (C) 2019-2020 AT&T Intellectual Property. All rights reserved.
+ *  Modifications Copyright (C) 2019-2021 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -144,7 +144,7 @@ public class JpaToscaServiceTemplateTest {
 
         tst.setToscaDefinitionsVersion(null);
         BeanValidationResult result = tst.validate("");
-        assertThat(result.getResult()).contains("toscaDefinitionsVersion").contains(Validated.IS_NULL);
+        assertThat(result.getResult()).contains("tosca_definitions_version").contains(Validated.IS_NULL);
 
         tst.setToscaDefinitionsVersion(JpaToscaServiceTemplate.DEFAULT_TOSCA_DEFINTIONS_VERISON);
         tst.setDataTypes(null);
