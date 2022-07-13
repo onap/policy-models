0ee2d997a118b4239c1a9d05fc3fd98028d0ba35
liamfallon <liam.fallon@est.tech>
2022-07-13 13:38:22 +0100

    Change scripts to sh rather than bash
    The models simulator in Honolulu uses bash for running scripts but bash
    is not available in the alpine base image.
    
    This commit replaces bash with sh
    
    Issue-ID: POLICY-4233
    Change-Id: I2cdab931257d6686c693d307c5eb742a896b0d08
    Signed-off-by: liamfallon <liam.fallon@est.tech>

--- models-sim/policy-models-simulators/src/main/package/docker/simulators.sh
+++ models-sim/policy-models-simulators/src/main/package/docker/simulators.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 #
 # ============LICENSE_START=======================================================
 # ONAP
 # ================================================================================
 # Copyright (C) 2020 AT&T Intellectual Property. All rights reserved.
 # Modifications copyright (C) 2020 Bell Canada. All rights reserved.
+# Modifications Copyright (C) 2022 Nordix Foundation.
 # ================================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
